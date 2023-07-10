e38e79723a7b63957d599128a34aa2a3ae678b57
Phuong Nguyen <phuongnguyen.cse@gmail.com>
2023-07-10 23:06:29 +0700

    Add data_slicing test case

--- starter/ml/data_slicing.py
+++ starter/ml/data_slicing.py
@@ -1,8 +1,12 @@
 import pandas as pd
 import numpy as np
+from sklearn.model_selection import train_test_split
+import pickle
 
+from data import process_data
+from model import inference, compute_model_metrics
 
-def data_slice(df, feature):
+def check_slice_stats(df, feature):
     for cls in df["salary"].unique():
         df_temp = df[df["salary"] == cls].copy()
 
@@ -19,12 +23,59 @@ def data_slice(df, feature):
         print(f"- {feature} stddev: {std:.4f}")
         print("\n")
 
+def check_performance_with_slice(df, feature):
+    _, test = train_test_split(df, test_size=0.2)
+    
+    with open("model/knn_model.pkl", 'rb') as f:
+        model = pickle.load(f)
 
+    with open("model/encoder.pkl", 'rb') as f:
+        encoder = pickle.load(f)
+
+    with open("model/lb.pkl", 'rb') as f:
+        lb = pickle.load(f)
+
+    cat_features = [
+        "workclass",
+        "education",
+        "marital-status",
+        "occupation",
+        "relationship",
+        "race",
+        "sex",
+        "native-country",
+    ]
+
+    with open("output/slice_output.txt", "w") as f:
+        for cls in test[feature].unique():
+            df_slice = test[test[feature]==cls]
+                
+            # encode data
+            X_test, y_test, _, _ = process_data(
+                X=df_slice,
+                categorical_features=cat_features,
+                label="salary",
+                encoder=encoder,
+                lb=lb,
+                training=False)
+
+            # inference
+            preds = inference(model, X_test)
+            
+            precision, recall, f1 = compute_model_metrics(y_test, preds)
+        
+            f.write(f'Feature: "{feature}" - <{cls}> \n')
+            f.write(f'>> Precision: {precision} \n')
+            f.write(f'>> Recall: {recall} \n')
+            f.write(f'>> F1: {f1} \n')
+        
 if __name__ == '__main__':
     path = "./data/census.csv"
     data = pd.read_csv(path, skipinitialspace=True)
 
-    data_slice(data, "education")
-    data_slice(data, "workclass")
-    data_slice(data, "marital-status")
-    data_slice(data, "occupation")
+    # check_slice_stats(data, "education")
+    # check_slice_stats(data, "workclass")
+    # check_slice_stats(data, "marital-status")
+    # check_slice_stats(data, "occupation")
+    
+    check_performance_with_slice(data, "education")
